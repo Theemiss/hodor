00c2168f9894963946b6fcd1e363b99cab24a280
Your Name <midinfotn401@gmail.com>
2021-01-04 15:32:39 +0100

    done

--- level_0/script.py
+++ level_0/script.py
@@ -10,7 +10,7 @@ cwd = os.getcwd()
 path = cwd + "/chromedriver"
 DRIVER_PATH = path
 options = Options()
-options.headless = False
+options.headless = True
 options.add_argument("--window-size=1400,900")
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
 Url = "http://158.69.76.135/level0.php"
@@ -19,5 +19,7 @@ for x in range(0, 1024):
     input_v = driver.find_element_by_name("id")
     input_v.send_keys("2152")
     input_v.send_keys(Keys.RETURN)
+    print(x,end="")
+    print(" voted")
     time.sleep(1)
 driver.quit()
---
+++ level_1/script.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import time
+from secretstorage import item
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import csv
+import os
+cwd = os.getcwd()
+path = cwd + "/chromedriver"
+DRIVER_PATH = path
+options = Options()
+options.headless = True
+options.add_argument("--window-size=1400,900")
+driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
+Url = "http://158.69.76.135/level1.php"
+driver.get(Url)
+for x in range(0, 4096):
+    input_v = driver.find_element_by_name("id")
+    input_v.send_keys("2152")
+    input_v.send_keys(Keys.RETURN)
+    print(x,end="")
+    print(" voted")
+    time.sleep(1)
+driver.quit()
